4d806f3a710cabce4dc4a3486be020dd63b9299a
rajneesh304 <singhatiya.rajneesh2@gmail.com>
2025-05-01 21:44:12 +0530

    implemented home page
    Implemented homepage with  custom navbar, footer and hero section.

---
+++ journal-ui/app/(marketing)/_components/heading.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+import React from "react";
+
+const Heading = () => {
+  return (
+    <div className="max-w-3xl space-y-4">
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+        Your ideas, documents & plans. Unified. Welcome to{" "}
+        <span className="underline">Lotion</span>
+      </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Lotion is connected workspace where <br />
+        better, faster work happens.
+      </h3>
+      <Button>
+        Enter Lotion
+        <ArrowRight className="h-4 w-4 ml-2" />
+      </Button>
+    </div>
+  );
+};
+
+export default Heading;
---
+++ journal-ui/app/(marketing)/_components/logo.tsx
@@ -0,0 +1,32 @@
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+import Image from "next/image";
+import React from "react";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600"],
+});
+const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image
+        src="/logo.svg"
+        height="40"
+        width="40"
+        alt="Logo"
+        className="dark:hidden"
+      />
+      <Image
+        src="/logo-dark.svg"
+        height="40"
+        width="40"
+        className="hidden dark:block"
+        alt="Logo"
+      />
+      <p className={cn("font-semibold", font.className)}>Lotion</p>
+    </div>
+  );
+};
+
+export default Logo;
