b04f93e50d470dbfebd291586c9188c4603e7638
rajneesh304 <singhatiya.rajneesh2@gmail.com>
2025-05-11 15:55:15 +0530

    implemented authentication
    with the help of convex as DB provider and clerk as auth provider

--- journal-ui/app/(marketing)/_components/heroes.tsx
+++ journal-ui/app/(marketing)/_components/heroes.tsx
@@ -5,7 +5,7 @@ const Heroes = () => {
   return (
     <div className="flex flex-col justify-center items-center max-w-5xl">
       <div className="flex items-center">
-        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
+        {/* <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
           <Image
             src="/documents.png"
             fill
@@ -32,7 +32,7 @@ const Heroes = () => {
             alt="Reading"
             className="object-contain hidden dark:block"
           />
-        </div>
+        </div> */}
       </div>
     </div>
   );
---
+++ journal-ui/convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://ample-bull-10.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
